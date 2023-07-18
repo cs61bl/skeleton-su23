08131eb9fe756469ebde0fce3d51d91205655e18
Anton Zabreyko <azabreyko@berkeley.edu>
2023-07-18 01:27:45 -0700

    release lab 14

---
+++ lab14/test/MinHeapPQTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+import static com.google.common.truth.Truth.assertWithMessage;
+
+
+public class MinHeapPQTest {
+
+    @Test
+    public void test1() {
+    }
+}
---
+++ lab14/test/MinHeapTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertWithMessage;
+
+public class MinHeapTest {
+
+    @Test
+    public void test1() {
+
+    }
+}
