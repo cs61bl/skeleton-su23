83b3c02444be5bce975710ec0d192f7cc3f9be90
Anton Zabreyko <azabreyko@berkeley.edu>
2023-07-19 09:35:52 -0700

    releasing lab 15

---
+++ lab15/test/UnionFindTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+import static com.google.common.truth.Truth.assertWithMessage;
+
+public class UnionFindTest {
+
+    @Test
+    public void test() {
+        UnionFind uf = new UnionFind(5);
+        // TODO: write tests!
+    }
+
+}
+
+
