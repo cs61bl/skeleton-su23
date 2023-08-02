8f2fd310299c4121fecc3dda5c3fe3388f9261ab
Anton Zabreyko <azabreyko@berkeley.edu>
2023-08-02 03:10:52 -0700

    release lab 22

---
+++ lab22/test/DistributionSortsTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static com.google.common.truth.Truth.assertThat;
+
+public class DistributionSortsTest {
+
+    @Test
+    public void testBasic() {
+        // TODO: test it out!
+    }
+    
+}
+
